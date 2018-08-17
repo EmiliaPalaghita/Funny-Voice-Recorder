455afa81c922ec53f3aef3cc5f386e9ec7244f74
epalaghita <p.emilia13@yahoo.com>
2018-08-17 12:05:36 +0300

    working version of app

--- app/src/main/java/com/example/epalaghita/funnyvoicerecorder/fragments/RecordFragment.kt
+++ app/src/main/java/com/example/epalaghita/funnyvoicerecorder/fragments/RecordFragment.kt
@@ -13,12 +13,15 @@ import android.widget.Button
 import com.example.epalaghita.funnyvoicerecorder.R
 import java.io.IOException
 import android.media.MediaPlayer
+import android.os.AsyncTask
 import android.os.Environment
 import android.os.SystemClock
 import android.support.v4.app.ActivityCompat
 import android.widget.Toast
+import com.example.epalaghita.funnyvoicerecorder.soundtouch.SoundTouch
 import com.example.epalaghita.funnyvoicerecorder.utils.RecordCallback
 import com.example.epalaghita.funnyvoicerecorder.utils.RecordPlayer
+import com.example.epalaghita.funnyvoicerecorder.utils.RecordPlayer.PATH
 import kotlinx.android.synthetic.main.record_fragment.*
 import java.io.File
 import java.util.*
@@ -51,28 +54,82 @@ class RecordFragment : Fragment() {
     }
 
     private fun startRecording() {
+
         record_button.setBackgroundResource(R.drawable.button_states_red)
         record_button.text = STOP_RECORD
 
-        mFileName = RecordPlayer.PATH + Date().time.toString() + ".mp3"
+        mFileName = RecordPlayer.PATH + Date().time.toString() + ".wav"
         val path: String = mFileName as String
+        Log.d("FILENAME", mFileName)
 
         RecordPlayer.startRecording(path)
+
+//        RecordPlayer.startRecordingWithAudioRecord(path)
 
         chronometer1.start()
         chronometer1.base = SystemClock.elapsedRealtime()
     }
 
     private fun stopRecording() {
+        val pitch = Integer.parseInt(pitch_editText.text.toString())
+        val speed = Integer.parseInt(speed_editText.text.toString())
+
         record_button.setBackgroundResource(R.drawable.button_states)
         record_button.text = START_RECORD
         RecordPlayer.stopRecording()
 
         chronometer1.stop()
         chronometer1.base = SystemClock.elapsedRealtime()
+
+//        RecordPlayer.stopRecordingWithAudioRecord()
+
+//        processRecording(pitch, speed)
+    }
+
+    private fun processRecording(pitch: Int, speed: Int) {
+        val processTask = ProcessTask()
+        val params: ProcessTask.Parameters = processTask.Parameters()
+        params.input = mFileName
+        params.output = PATH + "temp.wav"
+        params.speed = speed
+        params.pitch = pitch
+
+        Toast.makeText(this.context, "Starting to process file  $params.input ...", Toast.LENGTH_SHORT).show()
+
+        processTask.execute(params)
+    }
+
+    class ProcessTask : AsyncTask<ProcessTask.Parameters, Int, Long>() {
+
+
+        inner class Parameters {
+            var input: String? = ""
+            var output: String = ""
+            var speed = 100
+            var pitch = 0
+        }
+
+        private fun doSoundTouchProcessing(params: Parameters): Long {
+            val st = SoundTouch()
+            st.setPitchSemiTones(params.pitch.toFloat())
+            st.setSpeed(params.speed.toFloat())
+            val res = st.processFile(params.input, params.output)
+
+            if (res != 0) {
+                Log.e("FAILURE AT PROCESSING", SoundTouch.getErrorString())
+                return -1L
+            }
+
+            return 0L
+        }
+
+        override fun doInBackground(vararg p0: Parameters): Long {
+            return doSoundTouchProcessing(p0[0])
+        }
     }
 
     private fun playRecord() {
+
         play_button.text = STOP_PLAY
         try {
             val path: String = this.mFileName!!
@@ -82,6 +139,7 @@ class RecordFragment : Fragment() {
                 }
 
             })
+
         } catch (e: Exception) {
             Toast.makeText(this.context, "Try recording first!", Toast.LENGTH_SHORT).show()
         }
@@ -92,7 +150,9 @@ class RecordFragment : Fragment() {
 
         this.activity?.let {
             ActivityCompat.requestPermissions(it,
-                    arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    arrayOf(Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE),
                     REQUEST_RECORD_AUDIO_PERMISSION)
         }
 
--- app/src/main/java/com/example/epalaghita/funnyvoicerecorder/utils/RecordPlayer.kt
+++ app/src/main/java/com/example/epalaghita/funnyvoicerecorder/utils/RecordPlayer.kt
@@ -1,10 +1,10 @@
 package com.example.epalaghita.funnyvoicerecorder.utils
 
-import android.media.MediaPlayer
-import android.media.MediaRecorder
+import android.media.*
 import android.os.Environment
 import android.util.Log
-import java.io.IOException
+import java.io.*
+
 
 interface RecordCallback {
     fun onMediaPlayerFinished()
@@ -14,9 +14,15 @@ object RecordPlayer {
 
     val PATH = Environment.getExternalStorageDirectory().absolutePath + "/_audioDirectory/"
 
-    var mPlayer: MediaPlayer? = null
+    private var mPlayer: MediaPlayer? = null
     var startRecording: Boolean = true
-    var mRecorder: MediaRecorder? = null
+    private var mRecorder: MediaRecorder? = null
+    private var filePath: String = ""
+    private var recorder: AudioRecord? = null
+    private var recordingThread: Thread? = null
+    private var isRecording = false
+    val buffer = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_FLOAT)
+
 
     fun playRecord(path: String, completion: RecordCallback) {
         mPlayer = MediaPlayer()
@@ -30,7 +36,75 @@ object RecordPlayer {
         }
     }
 
+    fun startRecordingWithAudioRecord(path: String) {
+        recorder = AudioRecord(MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, buffer)
+        recorder?.startRecording()
+        isRecording = true
+        recordingThread = Thread(Runnable { writeAudioDataToFile(path) }, "AudioRecorder Thread")
+        recordingThread?.start()
+    }
+
+    fun playRecordWithAudioTrack(path: String, completion: RecordCallback) {
+        val file = File(path)
+        val minBuffer = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+        val byteArray = ByteArray(minBuffer)
+        val inputStream = FileInputStream(path)
+
+        val audioTrack = AudioTrack(AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build(),
+                AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setSampleRate(44100)
+                        .build(),
+                minBuffer, AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE)
+
+        audioTrack.play()
+        var i = inputStream.read(byteArray)
+
+        while (i != -1) {
+            audioTrack.write(byteArray, 0, i)
+            i = inputStream.read(byteArray)
+        }
+
+        completion.onMediaPlayerFinished()
+
+    }
+
+    private fun writeAudioDataToFile(path: String) {
+        val byteArray = ByteArray(buffer)
+        var os: FileOutputStream? = null
+        try {
+            os = FileOutputStream(path)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
+
+        while (isRecording) {
+            recorder?.read(byteArray, 0, buffer)
+            os?.write(byteArray, 0, buffer)
+
+        }
+        try {
+            os?.close()
+        } catch (e: IOException) {
+            e.printStackTrace();
+        }
+    }
+
+    fun stopRecordingWithAudioRecord() {
+        isRecording = false
+        recorder?.stop()
+        recorder?.release()
+        recorder = null
+        recordingThread = null
+    }
+
+
     fun startRecording(path: String) {
+        this.filePath = path
+
         if (mRecorder == null)
             mRecorder = MediaRecorder()
         mRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
@@ -64,4 +138,6 @@ object RecordPlayer {
             else -> true
         }
     }
+
+
 }
